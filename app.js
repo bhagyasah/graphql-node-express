b746f9dc4f7cbeb23bac140a85f36b7baf8e53da
Bhagya sah <bhagyasah4u@gmail.com>
2020-08-07 11:58:06 +0545

    making relationship between two schema working fine

--- app.js
+++ app.js
@@ -1,11 +1,132 @@
 const express = require('express');
 const bodyParsor = require('body-parser');
+const bcrypt = require('bcryptjs');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+const Event = require('./models/event');
+const User = require('./models/user');
 
 const app = express();
 app.use(bodyParsor.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello world !');
-})
+const events = [];
 
-app.listen(3000);
+app.use('/graphql', graphqlHTTP({
+  schema: buildSchema(`
+  type Event {
+    _id: ID!
+    title: String!
+    description: String!
+    price: Float!
+    date: String!
+  }
+
+  type User {
+    _id: ID!
+    email: String!
+    password: String
+  }
+
+  input EventInput {
+    title: String!
+    description: String!
+    price: Float!
+    date: String!
+  }
+
+  input UserInput {
+    email: String!
+    password: String!
+  }
+
+  type RootQuery {
+    events: [Event!]!
+  }
+
+  type RootMutation {
+    createEvent(eventInput: EventInput): Event
+    createUser(userInput: UserInput): User
+  }
+
+  schema {
+    query: RootQuery
+    mutation: RootMutation
+  }
+  `),
+  rootValue:{
+    events: () => {
+    return  Event.find().then((events) => {
+      console.log('Event List', events);
+      return events;
+      }).catch(e => {
+        throw e
+      })
+    },
+    createEvent: (args) => {
+      console.log('args in create Event', args);
+      const event = new Event({
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: new Date(args.eventInput.date),
+        creator: "5f2ce3a9872cd03555a5f447"
+      })
+      let createdEvent;
+     return event
+     .save()
+     .then((result) => {
+       createdEvent = {...result._doc};
+       User.findById('5f2ce3a9872cd03555a5f447')
+       .then(user => {
+         if (!user) {
+           throw new Error('User Not found')
+         }
+         user.createdEvents.push(event);
+         return user.save();
+       })
+       .then(result => {
+         return createdEvent;
+       })
+        console.log('result of save mon', result);
+        return{...result._doc};
+      }).catch((err) => {
+        console.log(err);
+        throw err;
+      });
+    },
+    createUser: args => {
+    return  User.findOne({ email: args.userInput.email}).then(user => {
+      if (user) {
+        throw new Error('User Exists already.')
+      }
+      return bcrypt.hash(args.userInput.password, 12);
+    })
+    .then((hashPassword) => {
+        const user = new User({
+          email: args.userInput.email,
+          password: hashPassword
+        });
+      return  user
+        .save()
+        .then(res =>{
+          return { ...res._doc }
+        })
+        .catch(err => console.log(err));
+      }).catch(err => {
+        console.log(err);
+        throw err;
+      })
+    }
+  },
+  graphiql: true,
+})
+)
+
+
+mongoose.connect(`mongodb+srv://bhagya:bhegr0MmWUp2Ulo6@cluster0.xjuf9.mongodb.net/test?retryWrites=true&w=majority`)
+.then((res) => {
+  app.listen(3000);
+}).catch((err) => {
+  console.error('Error on Mongoose', err);
+});
---
+++ test.js
@@ -0,0 +1,6 @@
+  const id = '1';
+  const name = 'Maruti Baleno';
+  const jpt1 = concat(name, id);
+//  const string = 'marutibaleno-1'
+
+  const strArray = string.split()
